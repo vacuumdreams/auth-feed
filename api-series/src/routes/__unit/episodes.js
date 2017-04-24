5baaea2d324f1f9b3fb2e464267a3f65a91cbd54
Botond Fekete <fekbot@gmail.com>
2017-04-24 09:44:30 +0300

    Created unit test file for series - episodes route

---
+++ api-series/src/routes/__unit/episodes.js
@@ -0,0 +1,10 @@
+'use strict'
+const {expect} = require('chai')
+const {after, before, describe, it} = require('mocha')
+const sinon = require('sinon')
+
+describe('Episodes route', () => {
+  it('should pass', () => {
+    expect(true).to.be.true
+  })
+})
